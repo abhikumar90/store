442c3f7c95c9d1bb5875fa6fb4a6949fdc885108
Abhishek <abhishek@kreatio.com>
2013-12-09 12:33:17 +0530

    store

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = 'dfe4655644f21112397ce1b4468aa6a4e8871147eb23887f73cbb41652ad6d556deadf5b7b9c256583018fe757ddcc750e61f1bbbf6fa86bf48b54896b59a4d3'
